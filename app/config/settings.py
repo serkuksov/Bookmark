ee14c1317268c2693e4ad111a39ac2501369e316
KuksovSV <ser.kuksov@mail.ru>
2023-10-30 21:48:59 +0300

    Добавлен докер+БД

--- app/config/settings.py
+++ app/config/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("DB_NAME", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
 
---
+++ app/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+poetry run python manage.py makemigrations --noinput
+poetry run python manage.py migrate --noinput
+#poetry run python manage.py collectstatic --noinput
+#poetry run gunicorn backpack_store.wsgi:application --bind 0.0.0.0:8000
+poetry run python manage.py runserver 0.0.0.0:8000
